05ff3d6dd6e1ec3432d2efe5774f09a51a0b81a0
Joshua Comeau <joshwcomeau@gmail.com>
2023-05-10 13:50:51 -0400

    Implement solution

---
+++ src/components/MainWrapper/MainWrapper.js
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react';
+import styled from 'styled-components';
+
+const MainWrapper = styled.main`
+  width: 100%;
+  max-width: 800px;
+  padding: 16px 24px;
+  margin: 0px auto;
+  border: 1px solid hsl(0deg 0% 50% / 0.3);
+  border-radius: 2px;
+  background: white;
+`;
+
+export default MainWrapper;
